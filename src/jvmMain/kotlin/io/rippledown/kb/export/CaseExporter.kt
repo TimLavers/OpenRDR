bfeb697322507d942ce0a10151b5fa0984755448
Tim Lavers <laverstim@gmail.com>
2022-11-26 15:56:21 +1100

    Export rule.

--- src/jvmMain/kotlin/io/rippledown/kb/export/CaseExporter.kt
+++ src/jvmMain/kotlin/io/rippledown/kb/export/CaseExporter.kt
@@ -7,7 +7,7 @@ import org.apache.commons.io.FileUtils
 import java.io.File
 import kotlin.text.Charsets.UTF_8
 
-class CaseExporter(val destination: File, val cases: Set<RDRCase>) {
+class CaseExporter(private val destination: File, val cases: Set<RDRCase>) {
     init {
         require(destination.exists()) {
             "Case export destination is not an existing directory."
