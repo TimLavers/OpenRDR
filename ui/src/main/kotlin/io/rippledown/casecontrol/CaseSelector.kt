0a97ddbfdc08d1649e93a5100e74be422a917461
Lindsay <lindsay.peters@bigpond.com>
2024-04-06 17:46:49 +1100

    - reduce gap between case names - test the scrolling of the case list

--- ui/src/main/kotlin/io/rippledown/casecontrol/CaseSelector.kt
+++ ui/src/main/kotlin/io/rippledown/casecontrol/CaseSelector.kt
@@ -2,18 +2,18 @@ package io.rippledown.casecontrol
 
 import androidx.compose.desktop.ui.tooling.preview.Preview
 import androidx.compose.foundation.VerticalScrollbar
+import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
-import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.requiredWidth
-import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.rememberLazyListState
 import androidx.compose.foundation.rememberScrollbarAdapter
 import androidx.compose.material.ExperimentalMaterialApi
-import androidx.compose.material.ListItem
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.testTag
 import androidx.compose.ui.semantics.contentDescription
 import androidx.compose.ui.semantics.semantics
@@ -33,7 +33,12 @@ fun CaseSelector(caseIds: List<CaseId>, handler: CaseSelectorHandler) {
     val count = caseIds.size
     val scrollState = rememberLazyListState()
 
-    Row {
+    Box(
+        modifier = Modifier
+            .fillMaxWidth()
+            .height(800.dp)
+            .background(Color.White)
+    ) {
         LazyColumn(
             state = scrollState,
             modifier = Modifier
@@ -45,20 +50,21 @@ fun CaseSelector(caseIds: List<CaseId>, handler: CaseSelectorHandler) {
         ) {
             items(count) { index ->
                 val caseId = caseIds[index]
-                ListItem(
+                Text(
+                    text = caseId.name,
                     modifier = Modifier
                         .clickable {
                             handler.selectCase(caseId.id!!)
                         }
                         .semantics { contentDescription = "$CASE_NAME_PREFIX${caseId.name}" },
-                    text = {
-                        Text(text = caseId.name)
-                    }
                 )
             }
         }
         VerticalScrollbar(
-            modifier = Modifier.requiredWidth(16.dp),
+            modifier = Modifier
+                .align(Alignment.CenterEnd)
+                .requiredWidth(10.dp)
+                .padding(end = 5.dp),
             adapter = rememberScrollbarAdapter(scrollState)
         )
     }
