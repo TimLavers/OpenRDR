4bbc01e05ecbcf4f55a4a91adc41532eec487ae4
Lindsay <lindsay.peters@bigpond.com>
2024-04-09 20:54:53 +1000

    - use a ToolTipArea rather than a custom component for the IntepretationTabs

--- ui/src/main/kotlin/io/rippledown/interpretation/InterpretationTabs.kt
+++ ui/src/main/kotlin/io/rippledown/interpretation/InterpretationTabs.kt
@@ -1,35 +1,27 @@
 @file:OptIn(ExperimentalComposeUiApi::class, ExperimentalComposeUiApi::class)
 
-import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
-import androidx.compose.foundation.hoverable
-import androidx.compose.foundation.interaction.MutableInteractionSource
 import androidx.compose.foundation.layout.*
-import androidx.compose.material.Surface
 import androidx.compose.material.Tab
 import androidx.compose.material.TabRow
-import androidx.compose.material.Text
 import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.ExperimentalComposeUiApi
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.painter.Painter
-import androidx.compose.ui.input.pointer.PointerEventType
-import androidx.compose.ui.input.pointer.onPointerEvent
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.semantics.contentDescription
 import androidx.compose.ui.semantics.semantics
 import androidx.compose.ui.semantics.testTag
-import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
-import androidx.compose.ui.unit.sp
 import io.rippledown.constants.interpretation.INTERPRETATION_TAB_CHANGES_LABEL
 import io.rippledown.constants.interpretation.INTERPRETATION_TAB_CONCLUSIONS_LABEL
 import io.rippledown.constants.interpretation.INTERPRETATION_TAB_ORIGINAL_LABEL
 import io.rippledown.interpretation.ConclusionsView
 import io.rippledown.interpretation.InterpretationView
 import io.rippledown.interpretation.InterpretationViewHandler
+import io.rippledown.interpretation.ToolTipForIconAndLabel
 import io.rippledown.model.diff.Diff
 import io.rippledown.model.interpretationview.ViewableInterpretation
 
@@ -41,6 +33,7 @@ interface InterpretationTabsHandler {
 
 //val lightBlueColor = Color(100, 149, 237)
 val lightBlueColor = Color(70, 130, 180)
+
 @Composable
 fun InterpretationTabs(viewableInterpretation: ViewableInterpretation, handler: InterpretationTabsHandler) {
     val titles = listOf(
@@ -50,8 +43,6 @@ fun InterpretationTabs(viewableInterpretation: ViewableInterpretation, handler: 
     )
 
     var tabPage by remember { mutableStateOf(0) }
-    val hoverList = remember { mutableStateListOf(false, false, false) }
-    val interactionSource = remember { MutableInteractionSource() }
 
     Column(modifier = Modifier
         .semantics { testTag = "tabs" }
@@ -66,42 +57,29 @@ fun InterpretationTabs(viewableInterpretation: ViewableInterpretation, handler: 
                 val isSelected = tabPage == index
                 Tab(
                     selected = isSelected,
-                    onClick = { tabPage = index },
+                    onClick = {}, // Do nothing, we handle this in the ToolTipForIconAndLabel
                     modifier = Modifier.semantics {
                         contentDescription = "interpretation_tab_$title"
                     }.background(Color.White)
                 ) {
                     Row(
                         verticalAlignment = Alignment.CenterVertically,
-                        horizontalArrangement = Arrangement.spacedBy(10.dp),
+                        horizontalArrangement = Arrangement.Start,
                         modifier = Modifier
                             .padding(5.dp)
-                            .onPointerEvent(PointerEventType.Enter) {
-                                hoverList[index] = true
-                            }
-                            .onPointerEvent(PointerEventType.Exit) {
-                                hoverList[index] = false
-                            }
-                            .hoverable(
-                                interactionSource = interactionSource,
-                            )
                     ) {
-                        Image(painter = painterForIndex(index), null)
-                        Text(
-                            text = title,
-                            fontWeight = if (isSelected) FontWeight.Bold else FontWeight.Normal,
-                            color = if (isSelected) Color.Blue else {
-                                if (hoverList[index] == true) lightBlueColor else Color.Black
-                            },
-                            fontSize = 16.sp,
+                        ToolTipForIconAndLabel(
+                            toolTipText = toolTipTextForIndex(index),
+                            labelText = title,
+                            iconContentDescription = "icon_content_description",
+                            isSelected = isSelected,
+                            icon = painterForIndex(index),
+                            onClick = { tabPage = index }
                         )
                     }
-
-
                 }
             }
         }
-        Tooltip(indexOfHoverPage(hoverList))
         when (tabPage) {
             0 -> {
                 InterpretationView(
@@ -132,34 +110,10 @@ fun painterForIndex(index: Int): Painter {
     }
 }
 
-@Composable
-fun Tooltip(index: Int) {
-    Surface(
-        color = Color.White,
-        contentColor = Color.Black,
-        modifier = Modifier
-            .height(20.dp)
-            .padding(horizontal = 40.dp)
-    ) {
-        if (index != -1) {
-            Box(
-                contentAlignment = Alignment.Center,
-            )
-            {
-                Text(
-                    text = when (index) {
-                        0 -> "The report given by the rules in the project. Edit it if not appropriate for this case."
-                        1 -> "The comments comprising the report, and the reasons why each comment is present."
-                        else -> "The differences between the original report and the one you edited. You can build a rule for each difference."
-                    },
-                    color = Color.Gray,
-                )
-            }
-        }
+fun toolTipTextForIndex(index: Int): String {
+    return when (index) {
+        0 -> "The report given by the rules in the project. Edit it if not appropriate for this case."
+        1 -> "The comments comprising the report, and the reasons why each comment is present."
+        else -> "The differences between the original report and the one you edited. You can build a rule for each difference."
     }
-}
-
-
-fun indexOfHoverPage(hoverList: List<Boolean>): Int {
-    return hoverList.indexOfFirst { it }
 }
---
+++ ui/src/main/kotlin/io/rippledown/interpretation/ToolTipForIconAndText.kt
@@ -0,0 +1,88 @@
+@file:OptIn(ExperimentalFoundationApi::class)
+
+package io.rippledown.interpretation
+
+import androidx.compose.foundation.*
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.Icon
+import androidx.compose.material.Surface
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.painter.Painter
+import androidx.compose.ui.semantics.contentDescription
+import androidx.compose.ui.semantics.semantics
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.DpOffset
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+
+val TOOLTIP_AREA_CONTENT_DESCRIPTION = "TOOLTIP_AREA"
+
+@OptIn(ExperimentalFoundationApi::class)
+@Composable
+fun ToolTipForIconAndLabel(
+    toolTipText: String,
+    labelText: String,
+    iconContentDescription: String,
+    isSelected: Boolean,
+    icon: Painter,
+    onClick: () -> Unit
+) {
+    TooltipArea(
+        modifier = Modifier
+            .semantics { contentDescription = TOOLTIP_AREA_CONTENT_DESCRIPTION },
+
+        tooltip = {
+            Surface(
+                color = Color.Gray,
+                shape = RoundedCornerShape(5.dp)
+            ) {
+                Text(
+                    text = toolTipText,
+                    modifier = Modifier.padding(5.dp),
+                    fontSize = 12.sp,
+                    color = Color.White
+                )
+            }
+        },
+        tooltipPlacement = TooltipPlacement.ComponentRect(
+            alignment = Alignment.BottomCenter,
+            offset = DpOffset(0.dp, 5.dp)
+        ),
+        content = {
+            Row(
+                modifier = Modifier
+                    .height(30.dp)
+                    .clickable {
+                        onClick()
+                    }
+                    .background(color = Color.White),
+                verticalAlignment = Alignment.CenterVertically,
+            ) {
+                Icon(
+                    icon,
+                    contentDescription = iconContentDescription,
+                    tint = Color.Black,
+                    modifier = Modifier
+                        .height(25.dp)
+                        .width(25.dp)
+                        .padding(5.dp)
+                )
+                Text(
+                    text = labelText,
+                    modifier = Modifier.padding(5.dp),
+                    fontWeight = if (isSelected) FontWeight.Bold else FontWeight.Normal,
+                    color = if (isSelected) Color.Blue else Color.Black,
+                    fontSize = 14.sp,
+                )
+            }
+        }
+    )
+}
