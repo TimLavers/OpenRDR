49fefdde339fa97bbef9c40ab6a757f898805ba3
Lindsay <lindsay.peters@bigpond.com>
2024-08-08 20:20:40 +1000

    - convert step definitions to be compatible with the Jetbrains Cucumber for Java plugin

--- cucumber/src/test/kotlin/steps/CaseViewStepDefs.kt
+++ cucumber/src/test/kotlin/steps/CaseViewStepDefs.kt
@@ -1,60 +1,64 @@
 package steps
 
 import io.cucumber.datatable.DataTable
-import io.cucumber.java8.En
+import io.cucumber.java.en.Then
 import io.kotest.matchers.shouldBe
 
-class CaseViewStepDefs : En {
-    init {
-        Then("I (should )see these episode dates:") { dataTable: DataTable ->
-            val expectedDates = dataTable.asList()
-            caseViewPO().datesShown() shouldBe expectedDates
-        }
+class CaseViewStepDefs {
+    @Then("I (should )see these episode dates:")
+    fun IShouldSeeTheseEpisodeDates(dataTable: DataTable) {
+        val expectedDates = dataTable.asList()
+        caseViewPO().datesShown() shouldBe expectedDates
+    }
 
-        Then("I (should )see these attributes:") { dataTable: DataTable ->
-            val expectedNames = dataTable.asList()
-            caseViewPO().attributeNames() shouldBe expectedNames
-        }
+    @Then("I (should )see these attributes:")
+    fun IShouldSeeTheseAttributes(dataTable: DataTable) {
+        val expectedNames = dataTable.asList()
+        caseViewPO().attributeNames() shouldBe expectedNames
+    }
 
-        Then("I (should )see these values for {string}:") { attribute: String, dataTable: DataTable ->
-            caseViewPO().valuesForAttribute(attribute) shouldBe dataTable.asList()
-        }
+    @Then("I (should )see these values for {string}:")
+    fun IShouldSeeTheseValuesForString(attribute: String, dataTable: DataTable) {
+        caseViewPO().valuesForAttribute(attribute) shouldBe dataTable.asList()
+    }
 
-        Then("I (should )see {string} as reference range for {string}") { range: String, attribute: String ->
-            caseViewPO().referenceRange(attribute) shouldBe range
-        }
+    @Then("I (should )see {string} as reference range for {string}")
+    fun IShouldSeeStringAsReferenceRangeForString(range: String, attribute: String) {
+        caseViewPO().referenceRange(attribute) shouldBe range
+    }
 
-        Then("I (should )see these case values:") { dataTable: DataTable ->
-            val caseViewPO = caseViewPO()
-            val valuesShown = caseViewPO.valuesShown()
-            val rowCount = valuesShown.size
-            // Check the number of rows is correct.
-            rowCount shouldBe dataTable.height()
-            // Extract the expected attributes, reference ranges and test result values.
-            val expectedAttributes = dataTable.column(0)
-            val referenceRangesExpected = dataTable.column(dataTable.width() - 1)
-            val expectedValues = dataTable.subTable(0, 1, rowCount, dataTable.width() - 1)
-            // Check each row.
-            valuesShown.keys.forEachIndexed { row, attribute ->
-                val expectedRow = expectedValues.row(row)
-                val rowShown = valuesShown[attribute]
-                attribute shouldBe expectedAttributes[row]
-                rowShown shouldBe expectedRow.map { it ?: "" }
-                val expectedReferenceRange = referenceRangesExpected[row].orEmpty()
-                val referenceRangeShown = caseViewPO.referenceRange(attribute)
-                expectedReferenceRange shouldBe referenceRangeShown
-            }
+    @Then("I (should )see these case values:")
+    fun IShouldSeeTheseCaseValues(dataTable: DataTable) {
+        val caseViewPO = caseViewPO()
+        val valuesShown = caseViewPO.valuesShown()
+        val rowCount = valuesShown.size
+        // Check the number of rows is correct.
+        rowCount shouldBe dataTable.height()
+        // Extract the expected attributes, reference ranges and test result values.
+        val expectedAttributes = dataTable.column(0)
+        val referenceRangesExpected = dataTable.column(dataTable.width() - 1)
+        val expectedValues = dataTable.subTable(0, 1, rowCount, dataTable.width() - 1)
+        // Check each row.
+        valuesShown.keys.forEachIndexed { row, attribute ->
+            val expectedRow = expectedValues.row(row)
+            val rowShown = valuesShown[attribute]
+            attribute shouldBe expectedAttributes[row]
+            rowShown shouldBe expectedRow.map { it ?: "" }
+            val expectedReferenceRange = referenceRangesExpected[row].orEmpty()
+            val referenceRangeShown = caseViewPO.referenceRange(attribute)
+            expectedReferenceRange shouldBe referenceRangeShown
         }
+    }
 
-        Then("the case should show the attributes in order:") { dataTable: DataTable ->
-            val caseViewPO = caseViewPO()
-            val valuesShown = caseViewPO.attributeNames()
-            // Check the number of rows is correct.
-            valuesShown.size shouldBe dataTable.height()
-            val expectedAttributes = dataTable.column(0)
-            valuesShown.forEachIndexed { row, attribute ->
-                attribute shouldBe expectedAttributes[row]
-            }
+    @Then("the case should show the attributes in order:")
+    fun theCaseShouldShowTheAttributesInOrder(dataTable: DataTable) {
+        val caseViewPO = caseViewPO()
+        val valuesShown = caseViewPO.attributeNames()
+        // Check the number of rows is correct.
+        valuesShown.size shouldBe dataTable.height()
+        val expectedAttributes = dataTable.column(0)
+        valuesShown.forEachIndexed { row, attribute ->
+            attribute shouldBe expectedAttributes[row]
         }
     }
 }
