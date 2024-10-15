b86ee2c7d46492db3d16f1bb14f3a4dbb0b77eb9
Lindsay <lindsay.peters@bigpond.com>
2024-10-15 17:44:37 +1100

    Removed unnecessary dependency

--- hints/build.gradle.kts
+++ hints/build.gradle.kts
@@ -1,7 +1,6 @@
 apply(from = "../repositories.gradle.kts")
 
 dependencies {
-    implementation(libs.bundles.kotlinx)
     implementation(libs.generativeai)
     implementation(libs.ktorClientMock)
     testImplementation(libs.kotestAssertions)
