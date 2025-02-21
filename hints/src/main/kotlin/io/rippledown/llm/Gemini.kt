3ee4e1cf160d9e33c042059075353873ba83f73a
Lindsay <lindsay.peters@bigpond.com>
2025-02-22 06:51:17 +1100

    insert prompt variables from files

--- hints/src/main/kotlin/io/rippledown/llm/Gemini.kt
+++ hints/src/main/kotlin/io/rippledown/llm/Gemini.kt
@@ -1,11 +1,8 @@
 package io.rippledown.llm
 
 import dev.shreyaspatil.ai.client.generativeai.GenerativeModel
+import dev.shreyaspatil.ai.client.generativeai.type.*
 import dev.shreyaspatil.ai.client.generativeai.type.BlockThreshold.NONE
-import dev.shreyaspatil.ai.client.generativeai.type.GenerationConfig
-import dev.shreyaspatil.ai.client.generativeai.type.HarmCategory
-import dev.shreyaspatil.ai.client.generativeai.type.SafetySetting
-import dev.shreyaspatil.ai.client.generativeai.type.content
 import io.rippledown.conditiongenerator.ConditionSpecification
 import io.rippledown.conditiongenerator.fromJson
 import kotlinx.coroutines.runBlocking
@@ -16,7 +13,6 @@ import java.lang.Thread.sleep
 import kotlin.random.Random.Default.nextLong
 
 const val GEMINI_MODEL = "gemini-1.5-flash"
-const val EXAMPLES_FILE = "/examples.txt"
 
 val logger: Logger = LoggerFactory.getLogger("rdr")
 
@@ -29,8 +25,12 @@ val generativeModel = GenerativeModel(
     safetySettings = noSafetySettings(),
     generationConfig = generativeConfig
 )
-val lines = object {}.javaClass.getResourceAsStream(EXAMPLES_FILE)?.bufferedReader()!!.readLines()
-val examples = examplesFrom(lines)
+
+val episodicPredicates = linesFrom("/prompt/episodic_predicates.txt").joinToString("\n")
+val episodicSignatures = linesFrom("/prompt/episodic_signatures.txt").joinToString("\n")
+val seriesPredicates = linesFrom("/prompt/series_predicates.txt").joinToString("\n")
+val caseStructurePredicates = linesFrom("/prompt/case_structure_predicates.txt").joinToString("\n")
+val examples = examplesFrom(linesFrom("/prompt/examples.txt"))
 
 fun noSafetySettings() =
     HarmCategory.entries
@@ -44,26 +44,32 @@ fun noSafetySettings() =
 
 fun conditionSpecificationFor(input: String): ConditionSpecification {
     val prompt = content {
+        text("---TASK SUMMARY---")
         text("Your task is to create a json object from an expression.")
+        text("---TASK DETAILS---")
         text("The json object should have two fields: predicate and signature.")
         text("The predicate field should have two fields: name and parameters.")
         text("The signature field should have two fields: name and parameters.")
         text("There are three types of predicate names: episodic, case structure and series.")
-        text("The possible values for an episodic predicate name are: Contains, DoesNotContain, Is, Low, High, Normal, GreaterThanOrEquals, LessThanOrEquals, IsNumeric.")
-        text("The possible values for a case structure predicate name are: IsAbsentFromCase, IsPresentInCase, IsSingleEpisodeCase.")
-        text("The possible values for a series predicate name are: Increasing, Decreasing.")
+        text("The possible values for an episodic predicate name are: $episodicPredicates.")
+        text("The possible values for a case structure predicate name are: $caseStructurePredicates.")
+        text("The possible values for a series predicate name are: $seriesPredicates.")
         text("The predicate name should be blank if the expression does not refer to one of the possible predicates.")
-        text("For an episodic predicate name, the possible values for the signature name are: All, Current, No, AtLeast, AtMost.")
-        text("For a case structure predicate name, the signature name should be blank.")
-        text("For a series predicate name, the signature name should be blank.")
+        text("For an episodic predicate name, the possible values for the signature name are: $episodicSignatures.")
+        text("Depending on the predicate, the signature may be an empty object '{}'.")
+        text("For a case structure predicate, the signature name should be blank.")
+        text("For a series predicate, the signature name should be blank.")
         text("There may be no parameter or just one parameter for the predicate.")
         text("There may be no parameter or just one parameter for the signature.")
+        text("---EXAMPLES---")
         text("Examples of expressions with the expected output are:")
         text(examples)
+        text("---INPUT EXPRESSION---")
         text("Here is the expression: $input")
-        text("Generate output without a leading ```json or trailing ```.")
+        text("---OUTPUT FORMAT---")
+        text("Generate output without a leading ```json and without a trailing ```.")
     }
-
+    printPrompt(prompt)
     val json = retry {
         runBlocking {
             generativeModel.generateContent(prompt).text
@@ -72,8 +78,15 @@ fun conditionSpecificationFor(input: String): ConditionSpecification {
     return if (json != null) fromJson(json) else ConditionSpecification()
 }
 
+private fun printPrompt(prompt: Content) {
+    prompt.parts.forEach { System.out.println(it.asTextOrNull()) }
+}
+
+private fun linesFrom(resourceFileName: String) =
+    object {}.javaClass.getResourceAsStream(resourceFileName)?.bufferedReader()!!.readLines()
+
 /**
- * Try to get around the 503 error from the API due to rate limiting.
+ * Retry when receiving the 503 error from the API due to rate limiting.
  */
 fun <T> retry(
     maxRetries: Int = 10,
@@ -92,5 +105,5 @@ fun <T> retry(
             currentDelay = (currentDelay * 2).coerceAtMost(maxDelay) + nextLong(0, 1_000)
         }
     }
-    throw IllegalStateException("Should not reach here")
+    throw IllegalStateException("Max retries of $maxRetries reached")
 }
